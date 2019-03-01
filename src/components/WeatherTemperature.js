7c0c45ac8524f3682762242b73fba7d01a0fea18
Alex Ballera <alexballera@gmail.com>
2019-02-28 21:50:02 -0300

    Inicial

---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import WeatherIcons from 'react-weathericons'
+
+const icons = {
+  sunny: 'day-sunny',
+  fog: 'day-fog'
+}
+
+const getWeatherIcons = weatherState => {
+  const icon = icons[weatherState]
+  if (icon)
+    return <WeatherIcons name={icon} size='2x' />
+  else
+    return <WeatherIcons name={'day-sunny'} size='2x' />
+}
+const WeatherTemperature = ({ temperature, weatherState }) => (
+  <div>
+    {
+      getWeatherIcons(weatherState)
+    }
+    <span>{`${temperature} Cº`}</span>
+  </div>
+)
+export default WeatherTemperature
